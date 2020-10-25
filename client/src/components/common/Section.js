b857a52bd9ae29100f95f2de483f998bc172415b
driverco <driverco@gmail.com>
2020-10-24 23:20:34 -0500

    texts, images and links

--- client/src/components/common/Section.js
+++ client/src/components/common/Section.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function imagePart(type, img) {
     return (
@@ -9,12 +10,13 @@ function imagePart(type, img) {
         </div>
     );
 }
-function textPart(type, title, content) {
+function textPart(type, title, content, link, linkText) {
     return (
         <div className={"col-lg-6 order-lg-" + (type === "0" ? "1" : "2")}>
             <div className="p-1">
                 <h2 className="display-4">{title}</h2>
                 <p>{content}</p>
+                <NavLink to={"./"+link} id={link} className="navLink">{linkText}</NavLink>
             </div>
         </div>
 
@@ -22,13 +24,14 @@ function textPart(type, title, content) {
 }
 
 
-function Section(type, title, content, img) {
+function Section(type, title, content, img, link, linkText) {
     return (
         <section>
             <div className="container">
                 <div className="row align-items-center">
+                
                     {imagePart(type, img)}
-                    {textPart(type, title, content)}
+                    {textPart(type, title, content, link, linkText)}
                 </div>
             </div>
         </section>
